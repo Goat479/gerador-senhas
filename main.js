1aafe2b7b5064a564ee92eeb9b0591f4f61a1c7d
Goat479 <bruno.henrique.verissimo@escola.pr.gov.br>
2026-08-05 11:32:39 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ numeroSenha.textContent = tamanhoSenha;
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
 
 function diminuiTamanho() {
     if (tamanhoSenha>1){
@@ -12,4 +14,28 @@ function diminuiTamanho() {
     }
    
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
+
+function aumentaTamanho() {
+    if (tamanhoSenha<20){
+        tamanhoSenha = tamanhoSenha + 1;
+    }
+   
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+geraSenha();
+
+function geraSenha(){
+    for (let i=0; i<tamanhoSenha; i++){
+        let numeroAleatorio = Math.random()* letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        console.log (letrasMaiusculas[numeroAleatorio]);
+    }    
+}
+
+campoSenha.value = letrasMaiusculas;
